2c9f6878f97f48c04b0b2955db15f9ad0942fc38
hzc <121660097@qq.com>
2016-11-10 09:30:48 +0800

    modify location from africa to normal

--- SearchMap/src/com/wust/search/MainActivity.java
+++ SearchMap/src/com/wust/search/MainActivity.java
@@ -12,6 +12,7 @@ import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
@@ -27,27 +28,35 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-	
+public class MainActivity extends Activity implements OnClickListener {
+
 	private MapView mMapView;
-	
+	private Button mLocButton;
+
 	// 百度地图对象
 	private BaiduMap mBaiduMap;
-	
+
 	// 定位服务的客户端
 	private LocationClient mLocClient;
-	
+
 	// 定位请求回调接口
 	private BDLocationListener mLocListener;
-	
+
 	// 定位图层显示方式 COMPASS,FOLLOWING,NORMAL
 	private LocationMode mCurrentMode;
 	
+	// 是否是第一次定位
+	private boolean isFirstLoc;
+	
+	
 	// 位图描述信息
 	private BitmapDescriptor mCurrentIcon;
-	
+
 	// 当前的经纬度
 	private double mCurrentLng;
 	private double mCurrentLat;
@@ -66,19 +75,51 @@ public class MainActivity extends Activity {
 		SDKInitializer.initialize(getApplicationContext());
 		setContentView(R.layout.activity_main);
 		// 获取地图控件引用
-		
+
 		initView();
+		 mBaiduMap
+         .setMyLocationConfigeration(new MyLocationConfiguration(
+                 mCurrentMode, true, mCurrentIcon));
+		initLoc();
+
+	}
+
+	// 视图初始化
+	private void initView() {
+		// 地图初始化
+		mMapView = (MapView) findViewById(R.id.bmapView);
+		mBaiduMap = mMapView.getMap();
+		
+		mLocButton = (Button) findViewById(R.id.button_location);
+
+		mLocButton.setOnClickListener(this);
+	}
+	
+	// 定位初始化
+	private void initLoc(){
+		isFirstLoc = true;
+		mLocClient = new LocationClient(this);
+		mLocListener = new MyLocationListener();
+        mLocClient.registerLocationListener(mLocListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+        mLocClient.setLocOption(option);
 		
 	}
 
-	
-
-	private void initView() {
+	@Override
+	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		mMapView = (MapView) findViewById(R.id.bmapView);
+		switch (v.getId()) {
+		case R.id.button_location:
+				
+			break;
+		default:
+			break;
+		}
 	}
-
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -91,13 +132,9 @@ public class MainActivity extends Activity {
 	 * 地图移动到我的位置,此处可以重新发定位请求，然后定位； 直接拿最近一次经纬度，如果长时间没有定位成功，可能会显示效果不好
 	 */
 	private void center2myLoc() {
-		System.out.println("-------1---------");
 		LatLng ll = new LatLng(mCurrentLat, mCurrentLng);
-		System.out.println("-------2---------");
 		MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
-		System.out.println("-------3---------");
 		mBaiduMap.animateMapStatus(u);
-		System.out.println("-------4---------");
 	}
 
 	@Override
@@ -113,56 +150,83 @@ public class MainActivity extends Activity {
 
 		return super.onOptionsItemSelected(item);
 	}
+
 	/**
 	 * 默认点击menu菜单，菜单项不现实图标，反射强制其显示
 	 */
 	@Override
-	public boolean onMenuOpened(int featureId, Menu menu)
-	{
+	public boolean onMenuOpened(int featureId, Menu menu) {
 
-		if (featureId == Window.FEATURE_OPTIONS_PANEL && menu != null)
-		{
-			if (menu.getClass().getSimpleName().equals("MenuBuilder"))
-			{
-				try
-				{
+		if (featureId == Window.FEATURE_OPTIONS_PANEL && menu != null) {
+			if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+				try {
 					Method m = menu.getClass().getDeclaredMethod(
 							"setOptionalIconsVisible", Boolean.TYPE);
 					m.setAccessible(true);
 					m.invoke(menu, true);
-				} catch (Exception e)
-				{
+				} catch (Exception e) {
 				}
 			}
 
 		}
 		return super.onMenuOpened(featureId, menu);
 	}
-	 @Override
-	 protected void onStart()
-	 {
-	 // 开启图层定位
-	 mBaiduMap.setMyLocationEnabled(true);
-	 if (!mLocClient.isStarted())
-	 {
-	 mLocClient.start();
-	 }
-	// // 开启方向传感器
-	// myOrientationListener.start();
-	 super.onStart();
-	 }
-	//
-	 @Override
-	 protected void onStop()
-	 {
-	 // 关闭图层定位
-	 mBaiduMap.setMyLocationEnabled(false);
-	 mLocClient.stop();
 	
-	 // 关闭方向传感器
-	// myOrientationListener.stop();
-	 super.onStop();
-	 }
+	private class MyLocationListener implements BDLocationListener{
+
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			// TODO Auto-generated method stub
+			 // map view 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                            // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                mCurrentLat = location.getLatitude();
+                mCurrentLng = location.getLongitude();
+                LatLng ll = new LatLng(mCurrentLat,
+                        mCurrentLng);
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
+
+        public void onReceivePoi(BDLocation poiLocation) {
+        }
+		
+	}
+
+	@Override
+	protected void onStart() {
+		// 开启图层定位
+		mBaiduMap.setMyLocationEnabled(true);
+		if (!mLocClient.isStarted()) {
+			mLocClient.start();
+		}
+		// // 开启方向传感器
+		// myOrientationListener.start();
+		super.onStart();
+	}
+
+	//
+	@Override
+	protected void onStop() {
+		// 关闭图层定位
+		mBaiduMap.setMyLocationEnabled(false);
+		mLocClient.stop();
+
+		// 关闭方向传感器
+		// myOrientationListener.stop();
+		super.onStop();
+	}
 
 	@Override
 	protected void onDestroy() {
